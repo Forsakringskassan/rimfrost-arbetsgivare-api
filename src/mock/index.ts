9d12dbcf52b3f3a2890da6a6636c85338bd4d11f
Tomas Bjerre <tomas.bjerre85@gmail.com>
2025-10-14 08:19:37 +0200

    chore: Initialize with git@github.com:Forsakringskassan/template-api.git tmpl/main 8030044acf466f5015133a5922a69be1bbc40e35

---
+++ src/mock/index.ts
@@ -0,0 +1,7 @@
+import alternatives from "./api/alternativs";
+import ombud from "./api/ombud";
+
+export { ombudResponse } from "./objects/Ombud";
+export { alternativesResponse } from "./objects/Alternatives";
+
+export const mockApi = [alternatives, ombud];
